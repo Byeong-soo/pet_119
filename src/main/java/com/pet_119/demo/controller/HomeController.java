b6e86f40374361426b6bd4f9fac50024fd8eac8e
ubs <ubs4939@naver.com>
2023-04-18 17:58:20 +0900

    h2_connect

--- src/main/java/com/pet_119/demo/controller/HomeController.java
+++ src/main/java/com/pet_119/demo/controller/HomeController.java
@@ -3,11 +3,17 @@ package com.pet_119.demo.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-
+    @GetMapping("/")
+    public String index() {
+        log.info("main page");
+        return "index";
+    }
 }
